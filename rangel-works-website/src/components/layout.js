e09118a4f183ac04d2c73ff7232876cfaca08b4c
neyneyalldayday <drfrankenstein123@gmail.com>
2021-04-19 17:19:38 -0500

    first gatsby commit

---
+++ rangel-works-website/src/components/layout.js
@@ -0,0 +1,48 @@
+
+import * as React from "react"
+import PropTypes from "prop-types"
+import { useStaticQuery, graphql } from "gatsby"
+import "./layout.css"
+import Navbar from "./Navbar"
+
+const Layout = ({ children }) => {
+  const data = useStaticQuery(graphql`
+    query SiteTitleQuery {
+      site {
+        siteMetadata {
+          title
+        }
+      }
+    }
+  `)
+
+  return (
+    <>
+     <Navbar />
+      <div
+        style={{
+          margin: `0 auto`,
+          maxWidth: 960,
+          padding: `0 1.0875rem 1.45rem`,
+        }}
+      >
+        <main>{children}</main>
+        <footer
+          style={{
+            marginTop: `2rem`,
+          }}
+        >
+          © {new Date().getFullYear()}, Built with
+          {` `}
+          <a href="https://www.gatsbyjs.com">Gatsby</a>
+        </footer>
+      </div>
+    </>
+  )
+}
+
+Layout.propTypes = {
+  children: PropTypes.node.isRequired,
+}
+
+export default Layout
---
+++ rangel-works-website/src/pages/works.js
@@ -0,0 +1,12 @@
+import * as React from "react"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const Works = () => (
+  <Layout>
+    <SEO title="Page two" />
+   <h1>Works</h1>
+  </Layout>
+)
+
+export default Works
