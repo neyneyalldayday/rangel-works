2010864f6968691ec39b8a3466b1dae58e8f058d
neyneyalldayday <drfrankenstein123@gmail.com>
2021-04-20 12:48:05 -0500

    navbar

--- rangel-works-website/src/components/Navbar/index.js
+++ rangel-works-website/src/components/Navbar/index.js
@@ -1,7 +1,16 @@
 import React, {useState , useEffect} from 'react';
 import { FaBars, FaTimes } from 'react-icons/fa'
 import { IconContext } from "react-icons/lib"
-import {Nav,NavbarContainer, NavLogo, NavIcon, MobileIcon} from './NavbarElements'
+import {
+    Nav,
+    NavbarContainer, 
+    NavLogo, 
+    NavIcon, 
+    MobileIcon,
+    NavMenu,
+    NavItem,
+    NavLinks
+} from './NavbarElements'
 
 const Navbar = () => {
     const[click, setClick] = useState(false)
@@ -35,6 +44,17 @@ const Navbar = () => {
                 <MobileIcon onClick={handleClick}>
                     {click ? <FaTimes /> : <FaBars />}
                 </MobileIcon>
+                <NavMenu onClick={handleClick} click={click}>
+                 <NavItem>
+                    <NavLinks to="/">Home</NavLinks>    
+                 </NavItem>   
+                 <NavItem>
+                    <NavLinks to="/images">Images</NavLinks>    
+                 </NavItem>   
+                 <NavItem>
+                    <NavLinks to="/about">About</NavLinks>    
+                 </NavItem>   
+                </NavMenu>
               </NavbarContainer>
             </Nav>
         </IconContext.Provider>
