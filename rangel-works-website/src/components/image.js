f603c3b2adeaadeb8a1c450435fc0b37bf798761
neyneyalldayday <drfrankenstein123@gmail.com>
2021-04-22 12:54:44 -0500

    broken again and always

--- rangel-works-website/src/components/image.js
+++ rangel-works-website/src/components/image.js
@@ -1,9 +1,43 @@
+import { graphql, useStaticQuery } from 'gatsby'
 import React from 'react'
-import { useStaticQuery, graphql } from "gatsby"
 import Img from "gatsby-image"
+import "./image.css"
 
 const Image = () => {
-    
+    const data = useStaticQuery(graphql`
+    query  {
+  allFile(
+    filter: {extension: {regex: "/(jpg)|(png)|(jpeg)/"}, name: {nin: ["background", "background2"]}}
+  ) {
+    edges {
+      node {
+        base
+        childImageSharp {
+          fluid{
+							...GatsbyImageSharpFluid
+          }
+          
+        }
+      }
+    }
+  }
+}
+
+    `)
+    return (
+       <div className="image-container">
+           <h1>Check out some cool shit</h1>
+           <div className="image-grid">
+               {data.allFile.edges.map((image, key) => (
+                 <Img key={key}
+                 className="image-item"
+                 fluid={image.node.childImageSharp.fluid}
+                 alt={image.node.base.split('.')[0]}
+                 />
+               ))}
+           </div>
+       </div>
+    )
 }
 
 export default Image
--- rangel-works-website/src/pages/index.js
+++ rangel-works-website/src/pages/index.js
@@ -1,12 +1,14 @@
 import * as React from "react"
 import HeroSection from "../components/HeroSection"
 import Layout from "../components/layout"
+import Image from "../components/image"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />  
     <HeroSection />
+    <Image />
   </Layout>
 )
 
