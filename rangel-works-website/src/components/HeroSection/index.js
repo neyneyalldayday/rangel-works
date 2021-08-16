8b80ca0aa8c00be76bfecd3f3d7fb1abc15ac160
neyneyalldayday <drfrankenstein123@gmail.com>
2021-08-16 13:48:47 -0500

    video being duuumb

--- rangel-works-website/src/components/HeroSection/index.js
+++ rangel-works-website/src/components/HeroSection/index.js
@@ -10,7 +10,9 @@ function HeroSection() {
         <div className="hero-container">
             <h1>Sarah Rangel Art</h1>
             <p>check it out</p>
-            
+            <video className="background-video">
+                <source src={Video} type="video/mp4"  autoPlay loop muted playsInline />
+            </video>                        
             <div className="hero-btns">
            <Button fontBig big primary as={Link} to="/works">Interested?</Button>
             </div>
